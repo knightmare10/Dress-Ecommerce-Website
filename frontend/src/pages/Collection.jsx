206f63a72a243c104003972bba059244156e4c30
soumya chattopadhyay <151312846+knightmare10@users.noreply.github.com>
2026-08-04 11:43:05 +0530

    add some new feature in collection.jsx

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,12 +1,54 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
 import Title from '../components/Title';
+import ProductItem from '../components/ProductItem';
+
 const Collection = () => {
 
   const { products } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
+  const [filterProducts, setFilterProducts] = useState([]);
+  const [category, setCategory] = useState([]);
+  const [subCategory, setSubCategory] = useState([]);
 
+  const toggleCategory = (e) =>{
+
+    if(category.includes(e.target.value)) {
+      setCategory(prev=> prev.filter(item => item !== e.target.value))
+    }
+    else{
+      setCategory(prev => [... prev,e.target.value])
+    }
+  }
+
+  const toggleSubCategory = (e) =>{
+
+    if(subCategory.includes(e.target.value)) {
+      setSubCategory(prev=> prev.filter(item => item !== e.target.value))
+    }
+    else{
+      setSubCategory(prev => [... prev,e.target.value])
+    }
+  }
+
+  const applyFilter = () =>{
+    let productsCopy = products.slice();
+
+    if(category.length > 0){
+      productsCopy = productsCopy.filter(item => category.includes(item.category));
+    }
+
+    setFilterProducts(productsCopy)
+  }
+
+  useEffect(()=>{
+    setFilterProducts(products);
+  },[])
+
+  useEffect(() =>{
+    applyFilter();
+  },[category, subCategory])
 
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 border-t'>
@@ -15,20 +57,21 @@ const Collection = () => {
         <p onClick={()=>setShowFilter(!showFilter)} className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS
           <img className={`h-3 sm:hidden ${showFilter ? 'rotate-90' : ''}`} src={assets.dropdown_icon} alt="" />
         </p>
+
         {/* Category Filter */}
         <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' : 'hidden'} sm:block`}>
         <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
         <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
           <p className='flex gap-2'>
-            <input className='w-3' type="checkbox" value={'Men'} /> Men
+            <input className='w-3' type="checkbox" value={'Men'} onChange={toggleCategory}/> Men
           </p>
 
           <p className='flex gap-2'>
-            <input className='w-3' type="checkbox" value={'Women'} /> Women
+            <input className='w-3' type="checkbox" value={'Women'} onChange={toggleCategory}/> Women
           </p>
 
           <p className='flex gap-2'>
-            <input className='w-3' type="checkbox" value={'Kids'} /> Kids
+            <input className='w-3' type="checkbox" value={'Kids'} onChange={toggleCategory}/> Kids
           </p>
         </div>
       </div>
@@ -39,15 +82,15 @@ const Collection = () => {
         <p className='mb-3 text-sm font-medium'>TYPE</p>
         <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
           <p className='flex gap-2'>
-            <input className='w-3' type="checkbox" value={'Topwear'} /> Topwear
+            <input className='w-3' type="checkbox" value={'Topwear'} onChange={toggleSubCategory}/> Topwear
           </p>
 
           <p className='flex gap-2'>
-            <input className='w-3' type="checkbox" value={'Bottomwear'} /> Bottomwear
+            <input className='w-3' type="checkbox" value={'Bottomwear'} onChange={toggleSubCategory}/> Bottomwear
           </p>
 
           <p className='flex gap-2'>
-            <input className='w-3' type="checkbox" value={'Winterwear'} /> Winterwear
+            <input className='w-3' type="checkbox" value={'Winterwear'} onChange={toggleSubCategory}/> Winterwear
           </p>
         </div>
       </div>
@@ -69,7 +112,11 @@ const Collection = () => {
 
         {/*Map Products */}
         <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6'>
-          
+          {
+            filterProducts.map((item, index)=>(
+              <ProductItem key={index} name={item.name} id={item._id} price={item.price} image={item.image}/>
+            ))
+          }
         </div>
       </div>
       
