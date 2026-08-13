af8f360a813555b85802159067f078bf84db9365
soumya chattopadhyay <151312846+knightmare10@users.noreply.github.com>
2026-08-13 20:33:42 +0530

    midfied product controller middleware and productroute

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,37 @@
 import validator from "validator";
-import userModel from "../models/userModel";
+import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken';
+import userModel from "../models/userModel.js";
 
+const createToken = (id) =>{
+    return jwt.sign({id}, process.env.JWT_SECRET)
+}
 
 //Route for user Login
 const loginUser = async (req, res)=>{
+    try {
+        const{email,password} = req.body;
 
+        const user = await userModel.findOne({email});
+
+        if(!user){
+            return res.json({success:false, message: "user doesn't exists"})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(isMatch){
+            
+            const token = createToken(user._id)
+            res.json({success:true, token})
+        }else{
+            res.json({success:false, message:"Invalid credentials"})
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:error.message})
+    }
 
 }
 
@@ -20,14 +47,32 @@ const registerUser = async (req, res)=>{
         }
 
         //validating email format & strong password
-        if(validator.isEmail(email)){
+        if(!validator.isEmail(email)){
             return res.json({success:false, message:"please enter a valid email"})
         }
         if(password.length < 8){
             return res.json({success:false, message:"please enter a strong password"})
         }
 
+        //hasing user password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt)
+
+        const newUser = new userModel({
+            name,
+            email,
+            password: hashedPassword
+        })
+
+        const user = await  newUser.save()
+
+        const token = createToken(user._id)
+
+        res.json({success:true, token})
+
     } catch (error) {
+        console.log(error);
+        res.json({success:false, message:error.message})
         
     }
 
@@ -36,6 +81,21 @@ const registerUser = async (req, res)=>{
 // Route for admin login
 const adminLogin = async(req, res)=>{
 
+    try {
+        
+        const { email, password} = req.body
+
+        if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
+            const token = jwt.sign(email+password, process.env.JWT_SECRECT);
+            res.json({success:true, token})
+        }else{
+            res.json({success: false, message:"Invalid credentials"})
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message})
+        
+    }
 
 }
 
