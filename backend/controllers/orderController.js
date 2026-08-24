250189f93a0928f4e5ca50750a7853d2f7962d0b
soumya chattopadhyay <151312846+knightmare10@users.noreply.github.com>
2026-08-24 23:00:08 +0530

    modified the controller page

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -76,7 +76,17 @@ const userOrders = async (req, res) => {
 
 // update order status from Admin Panel
 const upadteStatus = async (req, res) =>{
+    try {
+        const { orderId, status } = req.body
 
+        await orderModel.findByIdAndUpdate(orderId, { status })
+        res.json({success: true, message:'Status Updated'})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success: false , message:error.message})
+        
+    }
 }
 
 export {placeOrder, placeOrderPaytm, placeOrderGooglepay, allOrders, userOrders, upadteStatus}
